853fe89c9c2736f98e9819325666b5f3b08a9bda
Jakub <kubaxl222@gmail.com>
2023-03-01 23:42:56 +0100

    Add function that shows AI shoots missed and hits. This function shows them on the player gameboard

--- app/modules/battleLoop/battleLoop.js
+++ app/modules/battleLoop/battleLoop.js
@@ -4,30 +4,37 @@ const battleLoop=()=>{
     const aiGameBoardDivs=document.querySelectorAll(".gameBoardDivAI")
     const playerGameBoardDivs=document.querySelectorAll(".gameBoardDiv")
     const aiGameBoardDivsArray=Array.from(aiGameBoardDivs)
-    
+    let holderNumbers=[]
         aiGameBoardDivsArray.forEach(element => {
             element.addEventListener("click",()=>{
+                
                 let number=aiGameBoardDivsArray.indexOf(element).toString()
-                let coordinatesX;
-                let coordinatesY;
-                if(number>10){
-                    number.split(' ')
-                    coordinatesX=number[1]
-                    coordinatesY=number[0]
-                }else if(number==10){
-                    coordinatesX=0
-                    coordinatesY=1
+                if(holderNumbers.indexOf(+number)===-1){
+                    let coordinatesX;
+                    let coordinatesY;
+                    if(number>10){
+                        number.split(' ')
+                        coordinatesX=number[1]
+                        coordinatesY=number[0]
+                    }else if(number==10){
+                        coordinatesX=0
+                        coordinatesY=1
+                    }
+                    else{
+                        coordinatesX=number
+                        coordinatesY=0
+                    }
+                    holderNumbers.push(+number)
+                   ai.gameboard.receiveAttack(+coordinatesX,+coordinatesY)
+                    console.log(ai.gameboard.hitsMissed)
+                    aiGameBoardDivs[+number].style="background-color:green"
+                    showMissedShoot('AI')
+                   
+                    aiShoot()
                 }
-                else{
-                    coordinatesX=number
-                    coordinatesY=0
-                }
-               ai.gameboard.receiveAttack(+coordinatesX,+coordinatesY)
-                console.log(ai.gameboard.hitsMissed)
-                aiGameBoardDivs[+number].style="background-color:green"
-                showMissedShoot('AI')
+                // Add short info to user after clicked on the same gameboard div
+                else console.log("było")
                
-                aiShoot()
             })
         }
           
@@ -35,15 +42,31 @@ const battleLoop=()=>{
         );
     
    const aiShoot=()=>{
-    // console.log(ai.shoot())
+    let arr=ai.shoot()
+    let coordinatesX=arr[0]
+    let coordinatesY=arr[1]
+    let number=0
+    if(coordinatesY==0) number=coordinatesX
+    else number=(10*coordinatesY)+coordinatesX
+    
+    player.gameboard.receiveAttack(coordinatesX,coordinatesY)
+    playerGameBoardDivs[number].style="background-color:red"
+    showMissedShoot('player')
     
    }
-   const showMissedShoot=(player)=>{
-    if(player==='AI'){
+   const showMissedShoot=(user)=>{
+    if(user==='AI'){
         let array=ai.gameboard.hitsMissed
         for(let i=0;i<array.length;i++){
             aiGameBoardDivs[array[i]].innerHTML='x'
             aiGameBoardDivs[array[i]].style="background-color:blue"
+        }
+    }
+    else if(user==='player'){
+        let array=player.gameboard.hitsMissed
+        for(let i=0;i<array.length;i++){
+            playerGameBoardDivs[array[i]].innerHTML="x"
+            playerGameBoardDivs[array[i]].style="background-color:blue"
         }
     }
    }
